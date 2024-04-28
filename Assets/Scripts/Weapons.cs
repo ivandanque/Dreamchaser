b86e1c766293b566b81f29896814322fdef66ac2
BonjourPolaris <cole.d.balajadia@gmail.com>
2024-04-29 00:44:33 +0800

    Added ScriptableObject weapons

---
+++ Assets/Scripts/Weapons.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    [Header("Right Hand")]
+    public Weapon rightHandWeapon;
+    public KeyCode rightHandKey = KeyCode.Mouse0;
+
+    [Header("Left Hand")]
+    public Weapon leftHandWeapon;
+    public KeyCode leftHandKey = KeyCode.Mouse1;
+}
+
+[CreateAssetMenu(fileName = "New Weapon")]
+public class Weapon : ScriptableObject
+{
+    [TextArea]
+    public string description;
+    [Space]
+    public float damage;
+    public float basicAttackStartup;
+    public float basicAttackTime;
+    public float basicAttackRecovery;
+    [Space]
+    public float interruptValue;
+    public float basicAttackKnockbackIncrease;
+    public float spellInterruptIncrease;
+    public float basicAttackStunChance;
+}
