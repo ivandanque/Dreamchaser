6277ba465e24cae7010cd6e89ca93ec87d21fee3
ivandanque <ralphivan.danque.cics@ust.edu.ph>
2024-05-11 21:33:37 +0800

    Removed an unused scene, Added an SFX for unlocking portals, and switching scenes work
    Also I probably fcked sht up. Cole, tell me if I missed other stuff.
    I probably should've asked first lmao

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -10,6 +10,25 @@ public class Portal : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player")) OnLevelComplete?.Invoke(SceneManager.GetActiveScene().buildIndex + 1);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            OnLevelComplete?.Invoke(SceneManager.GetActiveScene().buildIndex + 1);
+
+            switch (SceneManager.GetActiveScene().name)
+            {
+                case "Level1":
+                    SceneManager.LoadScene("Level2");
+                    break;
+                case "Level2":
+                    SceneManager.LoadScene("Level3");
+                    break;
+                case "Level3":
+                    SceneManager.LoadScene("MainMenu");
+                    break;
+                default:
+                    SceneManager.LoadScene("MainMenu");
+                    break;
+            }
+        }
     }
 }
